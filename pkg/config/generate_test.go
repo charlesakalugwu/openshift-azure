7988b793538c9c29b722c217a3e01e2c467ce11a
charlesakalugwu <akalugwu@redhat.com>
2018-11-12 20:29:08 +0100

    add e2e tests for key rotation

--- pkg/config/generate_test.go
+++ pkg/config/generate_test.go
@@ -1,8 +1,11 @@
 package config
 
 import (
+	"reflect"
+	"strings"
 	"testing"
 
+	. "github.com/onsi/gomega"
 	"github.com/satori/go.uuid"
 
 	"github.com/openshift/openshift-azure/pkg/api"
@@ -125,4 +128,211 @@ func testRequiredFields(cs *api.OpenShiftManagedCluster, pc api.PluginConfig, t 
 	assert(c.AdminKubeconfig != nil, "AdminKubeconfig")
 	assert(c.NodeBootstrapKubeconfig != nil, "NodeBootstrapKubeconfig")
 	assert(c.AzureClusterReaderKubeconfig != nil, "AzureClusterReaderKubeconfig")
+}
+
+func TestGenerateServiceAccountKey(t *testing.T) {
+	RegisterTestingT(t)
+	t.Parallel()
+
+	var cg simpleGenerator
+	oc := fixtures.NewTestOpenShiftCluster()
+
+	Expect(oc).ToNot(BeNil())
+	oc.Config.ServiceAccountKey = nil
+	err := cg.Generate(oc)
+	Expect(err).To(BeNil())
+	Expect(oc.Config.ServiceAccountKey).NotTo(BeNil())
+	key := oc.Config.ServiceAccountKey
+	err = cg.Generate(oc)
+	Expect(err).To(BeNil())
+	Expect(reflect.DeepEqual(oc.Config.ServiceAccountKey, key)).To(BeTrue())
+}
+
+func TestGenerateWhileRunningUnderTest(t *testing.T) {
+	RegisterTestingT(t)
+	t.Parallel()
+
+	var cg simpleGenerator
+	oc := fixtures.NewTestOpenShiftCluster()
+
+	Expect(oc).ToNot(BeNil())
+	oc.Config.RunningUnderTest = true
+	oc.Config.HtPasswd = []byte("Y3VzdG9tZXItY2x1c3Rlci1hZG1pbjokMmEkMTAkdG1hQ1RZQVdYSVFtQ1VVMzFEOVpVZUlFSjQ0bmxENmdYeEFicTZGekUwOGo2S1Z2b0wub0sKY3VzdG9tZXItY2x1c3Rlci1yZWFkZXI6JDJhJDEwJGV0SnhmVG5XSWYuSUMyUGF4SThZbmUxbXFhUE5wcXlEcGdCWHFlaENERlQ4NGFSRHZuenAuCmVuZHVzZXI6JDJhJDEwJGRpY3BhU29aTjZObGp3N1IuOElNOHVIVDRDVmdhZ2djTVd0U1ZtWW1JWWkwVlJ5aS5FdTdxCg==")
+	oc.Config.CustomerAdminPasswd = ""
+	oc.Config.CustomerReaderPasswd = ""
+	oc.Config.EndUserPasswd = ""
+	err := cg.Generate(oc)
+	Expect(err).To(BeNil())
+	Expect(oc.Config.HtPasswd).NotTo(BeNil())
+	Expect(len(oc.Config.HtPasswd)).To(BeNumerically(">=", 0))
+	htpasswd := oc.Config.HtPasswd
+	err = cg.Generate(oc)
+	Expect(err).To(BeNil())
+	Expect(reflect.DeepEqual(oc.Config.HtPasswd, htpasswd)).To(BeTrue())
+}
+
+func TestGenerateSSHKey(t *testing.T) {
+	RegisterTestingT(t)
+	t.Parallel()
+
+	var cg simpleGenerator
+	oc := fixtures.NewTestOpenShiftCluster()
+
+	Expect(oc).ToNot(BeNil())
+	oc.Config.SSHKey = nil
+	err := cg.Generate(oc)
+	Expect(err).To(BeNil())
+	Expect(oc.Config.SSHKey).NotTo(BeNil())
+	key := oc.Config.SSHKey
+	err = cg.Generate(oc)
+	Expect(err).To(BeNil())
+	Expect(reflect.DeepEqual(oc.Config.SSHKey, key)).To(BeTrue())
+}
+
+func TestGenerateRegistryStorageAccount(t *testing.T) {
+	RegisterTestingT(t)
+	t.Parallel()
+
+	var cg simpleGenerator
+	oc := fixtures.NewTestOpenShiftCluster()
+
+	Expect(oc).ToNot(BeNil())
+	oc.Config.RegistryStorageAccount = ""
+	err := cg.Generate(oc)
+	Expect(err).To(BeNil())
+	Expect(oc.Config.RegistryStorageAccount).NotTo(BeEmpty())
+	name := oc.Config.RegistryStorageAccount
+	err = cg.Generate(oc)
+	Expect(err).To(BeNil())
+	Expect(reflect.DeepEqual(oc.Config.RegistryStorageAccount, name)).To(BeTrue())
+}
+
+func TestGenerateConfigStorageAccount(t *testing.T) {
+	RegisterTestingT(t)
+	t.Parallel()
+
+	var cg simpleGenerator
+	oc := fixtures.NewTestOpenShiftCluster()
+
+	Expect(oc).ToNot(BeNil())
+	oc.Config.ConfigStorageAccount = ""
+	err := cg.Generate(oc)
+	Expect(err).To(BeNil())
+	Expect(oc.Config.ConfigStorageAccount).NotTo(BeEmpty())
+	name := oc.Config.ConfigStorageAccount
+	err = cg.Generate(oc)
+	Expect(err).To(BeNil())
+	Expect(reflect.DeepEqual(oc.Config.ConfigStorageAccount, name)).To(BeTrue())
+}
+
+func TestGenerateLoggingWorkspace(t *testing.T) {
+	RegisterTestingT(t)
+	t.Parallel()
+
+	var cg simpleGenerator
+	oc := fixtures.NewTestOpenShiftCluster()
+
+	Expect(oc).ToNot(BeNil())
+	oc.Config.LoggingWorkspace = ""
+	err := cg.Generate(oc)
+	Expect(err).To(BeNil())
+	Expect(oc.Config.LoggingWorkspace).NotTo(BeEmpty())
+	name := oc.Config.LoggingWorkspace
+	err = cg.Generate(oc)
+	Expect(err).To(BeNil())
+	Expect(reflect.DeepEqual(oc.Config.LoggingWorkspace, name)).To(BeTrue())
+}
+
+func TestGenerateLoggingLocation(t *testing.T) {
+	RegisterTestingT(t)
+	t.Parallel()
+
+	var cg simpleGenerator
+	oc := fixtures.NewTestOpenShiftCluster()
+
+	Expect(oc).ToNot(BeNil())
+	oc.Config.LoggingLocation = ""
+	err := cg.Generate(oc)
+	Expect(err).To(BeNil())
+	Expect(oc.Config.LoggingLocation).NotTo(BeEmpty())
+	name := oc.Config.LoggingLocation
+	err = cg.Generate(oc)
+	Expect(err).To(BeNil())
+	Expect(reflect.DeepEqual(oc.Config.LoggingLocation, name)).To(BeTrue())
+}
+
+func TestGenerateRegistryConsoleOAuthSecret(t *testing.T) {
+	RegisterTestingT(t)
+	t.Parallel()
+
+	var cg simpleGenerator
+	oc := fixtures.NewTestOpenShiftCluster()
+
+	Expect(oc).ToNot(BeNil())
+	oc.Config.RegistryConsoleOAuthSecret = ""
+	err := cg.Generate(oc)
+	Expect(err).To(BeNil())
+	Expect(oc.Config.RegistryConsoleOAuthSecret).NotTo(BeEmpty())
+	Expect(strings.HasPrefix(oc.Config.RegistryConsoleOAuthSecret, "user")).To(BeTrue())
+	Expect(len(oc.Config.RegistryConsoleOAuthSecret)).To(Equal(68))
+	secret := oc.Config.RegistryConsoleOAuthSecret
+	err = cg.Generate(oc)
+	Expect(err).To(BeNil())
+	Expect(reflect.DeepEqual(oc.Config.RegistryConsoleOAuthSecret, secret)).To(BeTrue())
+}
+
+func TestGenerateConsoleOAuthSecret(t *testing.T) {
+	RegisterTestingT(t)
+	t.Parallel()
+
+	var cg simpleGenerator
+	oc := fixtures.NewTestOpenShiftCluster()
+
+	Expect(oc).ToNot(BeNil())
+	oc.Config.ConsoleOAuthSecret = ""
+	err := cg.Generate(oc)
+	Expect(err).To(BeNil())
+	Expect(oc.Config.ConsoleOAuthSecret).NotTo(BeEmpty())
+	Expect(len(oc.Config.ConsoleOAuthSecret)).To(Equal(64))
+	secret := oc.Config.ConsoleOAuthSecret
+	err = cg.Generate(oc)
+	Expect(err).To(BeNil())
+	Expect(reflect.DeepEqual(oc.Config.ConsoleOAuthSecret, secret)).To(BeTrue())
+}
+
+func TestGenerateRouterStatsPassword(t *testing.T) {
+	RegisterTestingT(t)
+	t.Parallel()
+
+	var cg simpleGenerator
+	oc := fixtures.NewTestOpenShiftCluster()
+
+	Expect(oc).ToNot(BeNil())
+	oc.Config.RouterStatsPassword = ""
+	err := cg.Generate(oc)
+	Expect(err).To(BeNil())
+	Expect(oc.Config.RouterStatsPassword).NotTo(BeEmpty())
+	Expect(len(oc.Config.RouterStatsPassword)).To(Equal(10))
+	pass := oc.Config.RouterStatsPassword
+	err = cg.Generate(oc)
+	Expect(err).To(BeNil())
+	Expect(reflect.DeepEqual(oc.Config.RouterStatsPassword, pass)).To(BeTrue())
+}
+
+func TestGenerateServiceCatalogClusterID(t *testing.T) {
+	RegisterTestingT(t)
+	t.Parallel()
+
+	var cg simpleGenerator
+	oc := fixtures.NewTestOpenShiftCluster()
+
+	Expect(oc).ToNot(BeNil())
+	oc.Config.ServiceCatalogClusterID = uuid.Nil
+	err := cg.Generate(oc)
+	Expect(err).To(BeNil())
+	Expect(uuid.Equal(oc.Config.ServiceCatalogClusterID, uuid.Nil)).To(BeFalse())
+	uuid := oc.Config.ServiceCatalogClusterID
+	err = cg.Generate(oc)
+	Expect(err).To(BeNil())
+	Expect(reflect.DeepEqual(oc.Config.ServiceCatalogClusterID, uuid)).To(BeTrue())
 }
