e73b004d47c795ba426410d339fdb41037ee52b4
charlesakalugwu <akalugwu@redhat.com>
2019-05-31 18:57:54 +0200

    Instrument azure controllers with metrics

--- pkg/entrypoint/azure-controllers/azure-controllers.go
+++ pkg/entrypoint/azure-controllers/azure-controllers.go
@@ -2,19 +2,34 @@ package azurecontrollers
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net"
 	"net/http"
 
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/sirupsen/logrus"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/runtime/signals"
 
+	"github.com/openshift/openshift-azure/pkg/api"
+	"github.com/openshift/openshift-azure/pkg/cluster"
 	"github.com/openshift/openshift-azure/pkg/controllers/customeradmin"
+	"github.com/openshift/openshift-azure/pkg/metrics"
+	"github.com/openshift/openshift-azure/pkg/util/cloudprovider"
+	"github.com/openshift/openshift-azure/pkg/util/configblob"
 	"github.com/openshift/openshift-azure/pkg/util/log"
 )
+
+func init() {
+	prometheus.MustRegister(metrics.AzureControllersInfoGauge)
+	prometheus.MustRegister(metrics.AzureControllersErrorsCounter)
+	prometheus.MustRegister(metrics.AzureControllersDurationSummary)
+	prometheus.MustRegister(metrics.AzureControllersInFlightGauge)
+	prometheus.MustRegister(metrics.AzureControllersLastExecutedGauge)
+}
 
 func start(cfg *cmdConfig) error {
 	ctx := context.Background()
@@ -22,7 +37,38 @@ func start(cfg *cmdConfig) error {
 	logrus.SetFormatter(&logrus.TextFormatter{FullTimestamp: true})
 	log := logrus.NewEntry(logrus.StandardLogger())
 
+	cpc, err := cloudprovider.Load("_data/_out/azure.conf")
+	if err != nil {
+		return err
+	}
+
+	bsc, err := configblob.GetService(ctx, log, cpc)
+	if err != nil {
+		return err
+	}
+
+	c := bsc.GetContainerReference(cluster.ConfigContainerName)
+	blob := c.GetBlobReference(cluster.SyncBlobName)
+
+	log.Print("reading config")
+	rc, err := blob.Get(nil)
+	if err != nil {
+		return err
+	}
+	defer rc.Close()
+
+	var cs *api.OpenShiftManagedCluster
+	err = json.NewDecoder(rc).Decode(&cs)
+	if err != nil {
+		return err
+	}
+
 	log.Print("azure-controller pod starting")
+	metrics.AzureControllersInfoGauge.With(prometheus.Labels{
+		"name":           "",
+		"image":          cs.Config.Images.AzureControllers,
+		"period_seconds": "",
+	}).Set(1)
 
 	// TODO(charlesakalugwu): Use controller-runtime's metrics exposition when
 	//  we are able to update controller-runtime to any versions > 0.1.0
